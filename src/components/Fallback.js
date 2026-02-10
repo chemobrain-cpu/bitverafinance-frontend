abfa9b212185d6b2560fec43965e3638088cec87
SkyNet <arierhiprecious@gmail.com>
2026-02-10 06:09:18 +0100

    first commit

---
+++ src/components/Fallback.js
@@ -0,0 +1,12 @@
+import styles from './Fallback.module.css';
+
+
+const Fallback = () => {
+    return (<div className={styles.container}>
+        <div>
+            
+        </div>
+    </div>)
+}
+
+export default Fallback
